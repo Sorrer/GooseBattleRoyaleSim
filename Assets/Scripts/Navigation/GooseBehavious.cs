dbe9bd0f07fdee8afde83d5b66e4f862dd5f004c
Bouncy Knight <utkarshjha329@gmail.com>
2019-04-20 18:22:52 -0400

    Simple Nav

---
+++ Assets/Scripts/Navigation/GooseBehavious.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GooseBehavious : MonoBehaviour
+{
+    public SimpleNavigation navEle;
+    public SphereCollider sC;
+    public GameObject thisChild;
+
+    public List<GameObject> gooseInRange = new List<GameObject>();
+    private bool gooseAdded = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gooseAdded)
+        {
+            SetToAttackGoose();
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Goose") && collision.gameObject != thisChild)
+        {
+            Debug.Log("Found a goose");
+            gooseInRange.Add(collision.gameObject);
+            gooseAdded = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (gooseInRange.Contains(other.gameObject))
+        {
+            gooseInRange.Remove(other.gameObject);
+        }
+    }
+
+    private void SetToAttackGoose()
+    {
+        if (gooseInRange.Count > 0)
+        {
+            int randIndex = Random.Range(0, gooseInRange.Count);
+            navEle.thisAgent.SetDestination(gooseInRange[randIndex].transform.position);
+            Debug.Log(gooseInRange[randIndex].transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/Navigation/SimpleNavigation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleNavigation : MonoBehaviour
+{
+    public NavMeshAgent thisAgent;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thisAgent.SetDestination(thisAgent.destination);
+        
+    }
+}
